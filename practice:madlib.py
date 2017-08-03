e2b0be38b8cb9fad0ca76804cbda56d70090f836
s203 <s203@AI241LAB0203M22.local>
2017-08-03 15:57:36 -0700

    lol k

---
+++ practice:madlib.py
@@ -0,0 +1,15 @@
+#madlib
+#Setup
+#Input
+print("Hello, please give me a noun")
+noun_one = input()
+
+print("Good, now give me a adjective.")
+adj_one = input()
+
+print("Okay, almost done, now give me one more noun.")
+noun_two = input()
+
+# Transform
+
+print(" The " + str(noun_one)  + " jumped over the "  + str(adj_one) + " " + str(noun_two) )
