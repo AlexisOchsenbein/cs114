3d2e40f9e3f01642a6c7010c4ca5bfaa93eaf727
s203 <s203@ai241lab0203m22.aipdcls.edu>
2017-08-17 15:10:45 -0700

    hallaaas

--- Magic8.py
+++ Magic8.py
@@ -16,25 +16,25 @@ if r == 1:
     print("Okay, " + str(name) + " Signs point to yes. ")
 
 if r == 2:
-    print( + str(name) + " Outlook good")
+    print("Okay, " + str(name) + " Outlook good")
 
 if r == 3:
-    print( + str(name) + " Very doubtful")
+    print("Okay, " + str(name) + " Very doubtful")
 
 if r == 4:
-    print( + str(name) + " Without a doubt")
+    print("Okay, " + str(name) + " Without a doubt")
 
 if r == 5:
-    print( + str(name) + " Yes, definitely")
+    print("Okay, " + str(name) + " Yes, definitely")
 
 if r == 6:
-    print( + str(name) + " It is decidedly so")
+    print("Okay, " + str(name) + " It is decidedly so")
 
 if r == 7:
-    print( + str(name) + " Don’t count on it")
+    print("Okay, " + str(name) + " Don’t count on it")
 
 if r == 8:
-    print( + str(name) + " My reply is no ")
+    print("Okay, " + str(name) + " My reply is no ")
 
 if r == 9:
-    print( + str(name) + " You may rely on it")
+    print("Okay, " + str(name) + " You may rely on it")
--- WritNumbs.py
+++ WritNumbs.py
@@ -4,18 +4,72 @@ num = int(input("A number between 1 and 99."))
 tens = num // 10 #First Numb
 ones = num % 10 #Second Numb
 if tens == 9:
-    tens_word = "ninety"
-elif tens == 8:
-    tens_word = "Eighty"
-elif tens == 7:
-    tens_word == "Seventy"
-elif tens == 6:
-    tens_word == "Sixty"
-elif tens == 5:
-    tens_word == "Fifty"
-elif tens == 4:
-    tens_word == "Fourty"
-elif tens == 3:
-    tens_word == "Thirty"
-elif tens == 2:
-    tens_word == "Twenty"
+    print("ninety")
+if tens == 8:
+    print("Eighty")
+if tens == 7:
+        print("Seventy")
+if tens == 6:
+        print("Sixty")
+if tens == 5:
+        print("Fifty")
+if tens == 4:
+        print("Fourty")
+if tens == 3:
+    print("Thirty")
+if tens == 2:
+    print("Twenty")
+
+if ones == 1:
+    if tens == 1:
+        print(" Eleven ")
+    else:
+        print( "Ten ")
+
+if ones == 2:
+    if tens == 1:
+        print("Twelve")
+    else:
+        print(" two")
+
+if ones == 3:
+    if tens == 1:
+        print("Thirteen")
+    else:
+            print(" three")
+
+if ones == 4:
+    if tens == 1:
+        print("Fourteen")
+    else:
+            print(" four")
+
+if ones == 5:
+    if tens == 1:
+        print("Fifteen")
+    else:
+            print(" five")
+
+if ones == 6:
+    if tens == 1:
+        print("Sixteen")
+    else:
+            print(" six ")
+
+if ones == 7:
+    if tens == 1:
+        print("Seventeen")
+    else:
+            print(" seven")
+
+if ones == 8:
+    if tens == 1:
+        print("Eightteen")
+    else:
+        print(" eight")
+
+if ones == 9:
+    if tens == 1:
+        print("Nineteen")
+    else:
+        print(" nine")
